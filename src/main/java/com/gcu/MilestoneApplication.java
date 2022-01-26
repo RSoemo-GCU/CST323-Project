a257f4df9e687a76d6df9f1b0d4d7954b98a9555
RSoemo <77287690+RSoemo-GCU@users.noreply.github.com>
2022-01-26 08:44:31 -0800

    Release 2.0
     - Fixed layout template problems
     - Configured the project to be easily exported

---
+++ src/main/java/com/gcu/MilestoneApplication.java
@@ -0,0 +1,18 @@
+/* Created by Rhese Soemo */
+/* For CST-323 Cloud Computing */
+/* On 1/7/2021 */
+/* This file was automatically generated */
+
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilestoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilestoneApplication.class, args);
+	}
+
+}
